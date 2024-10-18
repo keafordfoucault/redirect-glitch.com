c483b761d634ee8c82f46a5b42a175727c82ab00
keafordfoucault <keafordfoucault@gmail.com>
2024-10-18 16:29:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 // Chuyển hướng với query string
 app.get("/", (req, res) => {
   const queryString = req.url.split("?")[1];
-  const redirectUrl = `https://www.examplse.com${queryString ? "?" + queryString : ""}`;
+  const redirectUrl = `https://www.example.com${queryString ? "?" + queryString : ""}`;
   res.redirect(301, redirectUrl);
 });
 
-// Lắng nghe trên cổng mà Glitch yêu cầu
-const listener = app.listen(process.env.PORT, () => {
+// Lắng nghe kết nối
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
